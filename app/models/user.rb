02ce7829494de94eef451c7c274bfa8440b016c3
Guest User <Guest@Epicodus-2A.local>
2017-11-29 10:32:57 -0800

    get user auth working again

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ApplicationRecord
   attr_accessor :password
-  validates_confirmation_of :password, :uniqueness => true
-  validates :username, :presence => true, :uniqueness => true
-  validates :password_salt, :presence => true
-  validates :password_hash, :presence => true
-  validates :email, :presence => true
+  validates_confirmation_of :password
+  validates :email, :presence => true, :uniqueness => true
   before_save :encrypt_password
-
   def encrypt_password
     self.password_salt = BCrypt::Engine.generate_salt
     self.password_hash = BCrypt::Engine.hash_secret(password,password_salt)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 describe User do
-  it { should validate_presence_of :username }
   it { should validate_presence_of :email }
-  it { should validate_uniqueness_of :username }
   it { should validate_confirmation_of :password }
-  it { should validate_presence_of :password_hash }
-  it { should validate_presence_of :password_salt }
 end
